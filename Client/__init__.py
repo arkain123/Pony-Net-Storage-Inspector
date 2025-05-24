6e61c60c19b4f5666f0e6d0fbface6579c2b2ec4
arkain123 <dimabatkabatka14@gmail.com>
2025-05-24 03:44:44 +0300

    F server files

--- Client/__init__.py
+++
@@ -1,5 +0,0 @@
-from .local_info import LocalSystemInfo
-from .client import DiskUsageClient
-from .gui import DiskUsageApp, run_gui
-
-__all__ = ['LocalSystemInfo', 'DiskUsageClient', 'DiskUsageApp', 'run_gui']
